9623547f5ab56b0833a68b83cbae44d46aa45a64
arpitbbhayani <arpit.b.bhayani@gmail.com>
2014-08-16 22:02:39 +0530

    Hello Tweetie

---
+++ src/com/dataclox/tweetie/main/Tweetie.java
@@ -0,0 +1,12 @@
+package com.dataclox.tweetie.main;
+
+/**
+ * Created by devilo on 16/8/14.
+ */
+public class Tweetie {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Tweetie !!");
+    }
+
+}
